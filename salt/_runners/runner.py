219d52161d2dfada9b374b720d104964d37891b0
rpadwal <rpadwal@xento.com>
2021-02-16 17:59:14 +0530

    Runner changes

--- salt/_runners/runner.py
+++ salt/_runners/runner.py
@@ -8,4 +8,4 @@ def up():
     client = salt.client.LocalClient(__opts__['conf_file'])
     minions = client.cmd('*', 'test.ping', timeout=1)
     for minion in sorted(minions):
-        print minion
+        print ( minion ) 
